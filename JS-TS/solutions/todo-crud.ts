d2345530e2b876db8e0c216a5bc48ed09a3c1333
Ystaw <kristinkasvich@gmail.com>
2026-03-06 15:59:59 +0300

    task 4 is done

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,46 @@
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  if (!todo) {
+    throw new Error("Todo not found");
+  }
+  return [...state, todo];
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+
+  const todo = state.find(t => t.id === id);
+
+  if (!todo) {
+    throw new Error("Todo not found");
+  }
+
+  return state.map(todo => {
+    if (todo.id === id) {
+      return { ...todo, ...update };
+    }
+    return todo;
+  }
+  )
+
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  const todo = state.find(t => t.id === id);
+
+  if (!todo) {
+    throw new Error("Todo not found");
+  }
+
+  return state.filter(todo => todo.id !== id)
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  const todo = state.find(t => t.id === id);
+
+  if (!todo) {
+    throw new Error("Todo not found");
+  }
+
+  return todo;
 }
