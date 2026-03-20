aaf66f84e51b757cf4eee263b722f87c1c7f4994
Ystaw <kristinkasvich@gmail.com>
2026-03-20 03:05:24 +0300

    task 4 node-express-nest is  done

--- Node-Express-Nest/v0/solutions/task-04.js
+++ Node-Express-Nest/v0/solutions/task-04.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const app = express();
 
-// TODO: implement todos storage and GET /todos/:id logic
-
+app.get('/todos/:id', (req, res) => {
+    const todos = [{ id: 1, title: 'Buy milk', completed: false }];
+    const id = Number(req.params.id);
+    const todo = todos.find(t => t.id === id);
+    if (!todo) {
+        res.status(404).send('Todo not found');
+    }
+    res.send(todo);
+})
 module.exports = app; 
