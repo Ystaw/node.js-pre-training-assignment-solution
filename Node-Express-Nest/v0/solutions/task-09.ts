6dacca22863aedd80e66e2f47ab71d28e360a92d
Ystaw <kristinkasvich@gmail.com>
2026-03-23 15:37:32 +0300

    task 9 node-express-nest is done

--- Node-Express-Nest/v0/solutions/task-09.ts
+++ Node-Express-Nest/v0/solutions/task-09.ts
@@ -1,4 +1,14 @@
-// NestJS DTO class for ToDo
+// NestJS DTO class for 
+
 export class ToDoDto {
   // TODO: implement fields: id, title, completed
+  id: number;
+  title: string;
+  completed: boolean;
+
+  constructor(id: number, title: string, completed: boolean) {
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+  }
 } 
