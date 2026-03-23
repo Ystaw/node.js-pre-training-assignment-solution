aa992166bd849232ec827a7588671e7939eb2780
Ystaw <kristinkasvich@gmail.com>
2026-03-23 14:55:19 +0300

    task 6 node-express-nest is full done

--- Node-Express-Nest/v0/solutions/task-05.ts
+++ Node-Express-Nest/v0/solutions/task-05.ts
@@ -1,10 +1,12 @@
 // NestJS Controller for /todos
 import { Controller, Get } from '@nestjs/common';
+import { TodosService } from './task-06';
 
 @Controller('todos')
 export class TodosController {
+  constructor(private todosService: TodosService) { };
   @Get()
   getTodos() {
-    return [{ id: 1, title: 'Buy milk', completed: false }];
+    return this.todosService.getTodos();
   }
 } 
