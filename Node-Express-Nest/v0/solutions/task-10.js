ae76f76dff3da1f1895e6055739dcc9dd543a5fa
Ystaw <kristinkasvich@gmail.com>
2026-03-23 15:58:52 +0300

    task 10 node-express-nest is done

--- Node-Express-Nest/v0/solutions/task-10.js
+++ Node-Express-Nest/v0/solutions/task-10.js
@@ -1,3 +1,17 @@
 // Express.js GET /todos/search endpoint with query params
 // TODO: implement
-module.exports = {}; 
+
+const express = require('express');
+const app = express();
+
+app.get('/todos/search', (req, res) => {
+    const { completed } = req.query;
+    if (completed === undefined) {
+        return res.json(todos);
+    }
+    const isCompleted = completed === 'true';
+    const filteredTodos = todos.filter(todo => todo.completed === isCompleted);
+    res.json(filteredTodos);
+})
+
+module.exports = app; 
